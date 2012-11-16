a3423e85f21fb7bb114078fe1d0a23a61fdef77c
Helen Cook <helen.cook@gmail.com>
2012-11-16 18:21:46 +0100

    and now we have a working controller to display the 10 lastest genomes (by genome_id)

--- genome_atlas/displaygenome/models.py
+++ genome_atlas/displaygenome/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Genome_Stats(models.Model):
-    genome = models.IntegerField(primary_key=True)
+    genome_id = models.IntegerField(primary_key=True)
     tax_id = models.IntegerField()
     bioproject_id = models.IntegerField()
     genome_name = models.CharField(max_length=200)
--- genome_atlas/displaygenome/views.py
+++ genome_atlas/displaygenome/views.py
@@ -1,8 +1,16 @@
 # Create your views here.
+from django.template import Context, loader
 from django.http import HttpResponse
+from displaygenome.models import Genome_Stats
 
 def index(request):
-    return HttpResponse("This is the list of all the genomes.")
+    latest_genome_list = Genome_Stats.objects.all().order_by("-genome_id")[:10]
+
+    t = loader.get_template('genomes/index.html')
+    c = Context({
+        'latest_genome_list': latest_genome_list,
+    })
+    return HttpResponse(t.render(c))
 
 def detail(request, genome_id):
     return HttpResponse("You're looking at genome %s." % genome_id)
