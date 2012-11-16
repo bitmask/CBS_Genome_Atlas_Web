a6e0d187e2a61d42f1e785224b7b8639a2c92f73
Helen Cook <helen.cook@gmail.com>
2012-11-16 17:07:39 +0100

    initial set up of some models to show the genome_stats and replicons

--- genome_atlas/displaygenome/models.py
+++ genome_atlas/displaygenome/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Genome_Stats(models.Model):
+    genome = models.IntegerField(primary_key=True)
+    tax_id = models.IntegerField()
+    bioproject_id = models.IntegerField()
+    genome_name = models.CharField(max_length=200)
+    score = models.FloatField()
+    chromosome_count = models.IntegerField()
+    plasmid_count = models.IntegerField()
+    total_bp = models.IntegerField()
+    gene_count = models.IntegerField()
+    percent_at = models.FloatField()
+
+    def __unicode__(self):
+        return self.genome_id
+    
+class Replicon(models.Model):
+    replicon_id = models.IntegerField(primary_key=True)
+    accession = models.CharField(max_length=200)
+    version = models.IntegerField()
+    genome = models.ForeignKey(Genome_Stats)
+    replicon_type = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.accession
